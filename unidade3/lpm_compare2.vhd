d679cd7e67567519501b6982833c57493b99316b
Igor Macedo <igormacedo94@yahoo.com.br>
2017-06-20 12:07:43 -0300

    almost done unidade 3

--- unidade3/lpm_compare2.vhd
+++ unidade3/lpm_compare2.vhd
@@ -71,7 +71,7 @@ ARCHITECTURE SYN OF lpm_compare2 IS
 	END COMPONENT;
 
 BEGIN
-	sub_wire1_bv(30 DOWNTO 0) <= "0000000000000001100001101010000";
+	sub_wire1_bv(30 DOWNTO 0) <= "0000000000000000110000110101000";
 	sub_wire1    <= To_stdlogicvector(sub_wire1_bv);
 	alb    <= sub_wire0;
 
@@ -104,7 +104,7 @@ END SYN;
 -- Retrieval info: PRIVATE: INTENDED_DEVICE_FAMILY STRING "Cyclone II"
 -- Retrieval info: PRIVATE: LPM_PIPELINE NUMERIC "0"
 -- Retrieval info: PRIVATE: Latency NUMERIC "0"
--- Retrieval info: PRIVATE: PortBValue NUMERIC "50000"
+-- Retrieval info: PRIVATE: PortBValue NUMERIC "25000"
 -- Retrieval info: PRIVATE: Radix NUMERIC "10"
 -- Retrieval info: PRIVATE: SYNTH_WRAPPER_GEN_POSTFIX STRING "0"
 -- Retrieval info: PRIVATE: SignedCompare NUMERIC "0"
@@ -121,7 +121,7 @@ END SYN;
 -- Retrieval info: USED_PORT: alb 0 0 0 0 OUTPUT NODEFVAL "alb"
 -- Retrieval info: USED_PORT: dataa 0 0 31 0 INPUT NODEFVAL "dataa[30..0]"
 -- Retrieval info: CONNECT: @dataa 0 0 31 0 dataa 0 0 31 0
--- Retrieval info: CONNECT: @datab 0 0 31 0 50000 0 0 31 0
+-- Retrieval info: CONNECT: @datab 0 0 31 0 25000 0 0 31 0
 -- Retrieval info: CONNECT: alb 0 0 0 0 @alb 0 0 0 0
 -- Retrieval info: GEN_FILE: TYPE_NORMAL lpm_compare2.vhd TRUE
 -- Retrieval info: GEN_FILE: TYPE_NORMAL lpm_compare2.inc FALSE
